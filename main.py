ee5ef969ee1d6ed6363eed0bdf4f4680da480908
ISalimzhanov <iskabaur@gmai..com>
2020-11-28 22:50:23 +0600

    get profile data about person

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from financial_profile.providers.fsspProvider import FsspProvider
 from financial_profile.providers.terroristProvider import TerroristProvider
 
 if __name__ == '__main__':
-    os.environ["token"] = "W9kswdsrfD2J"
     providers: list = [FsspProvider, TerroristProvider]
     data = {"firstname": "Иван", "lastname": "Иванов", "region": 1, "birthdate": "01.09.1962"}
     for provider in providers:
