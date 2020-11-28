98f936f8c83fda318b1dd0b72f1d8d988759297b
ISalimzhanov <iskabaur@gmai..com>
2020-11-28 16:59:45 +0600

    parsed https://kad.arbitr.ru/

--- parser/arbitr.py
+++ parser/arbitr.py
@@ -1,48 +1,92 @@
 from bs4 import BeautifulSoup
 import requests
 import os
+import json
 
 
-def parse_civil():
+def parse(fname: str):
     def find_plaintiff(tag):
         return tag.has_attr("class") and tag["class"] == ["plaintiff"]
 
-    def find_court(tag):
+    def find_respondent(tag):
         return tag.has_attr("class") and tag["class"] == ["respondent"]
 
     def find_data(tag):
         return tag.has_attr("class") and tag["class"] == ["js-rolloverHtml"]
 
-    def filter(line: str) -> str:
+    def find_court(tag):
+        return tag.has_attr("class") and tag["class"] == ["court"]
+
+    def find_date(tag):
+        return tag.has_attr("class") and tag["class"] == ["num"]
+
+    def clean(line: str) -> str:
         res = ""
         l = 0
-        while l < len(line) and line[l] == ' ':
+        acceptable_symbols = [' ', '.']
+        for c in line:
+            if c.isalnum() or c.isalpha() or c in acceptable_symbols:
+                res += c
+        while l < len(res) and res[l] == ' ':
             l += 1
-        r = len(line) - 1
-        while r >= 0 and line[r] == ' ':
+        r = len(res) - 1
+        while r >= 0 and res[r] == ' ':
             r -= 1
-        for i in range(l, r):
-            if line[i].isalnum() or line[i].isalpha() or line[i] == ' ':
-                res += line[i]
+        res = res[l: r + 1]
         return res
 
-    with open("civil.html", "rb") as f:
+    def involved_info(tag):
+        try:
+            inv_data = tag.find(find_data).get_text()
+            res = []
+            for d in inv_data.split('\n'):
+                filtered = clean(d)
+                if filtered != "":
+                    res.append(filtered)
+            if len(res) < 3:
+                while len(res) < 3:
+                    res.append(None)
+            return res
+        except AttributeError:
+            return [None, None, None]
+
+    data = {"respondent": [], "plaintiff": [], "court": [], "date": []}
+
+    with open(fname, "rb") as f:
         content = f.read()
         soup = BeautifulSoup(content, "html.parser")
         entries = soup.find_all("tr")
-        data = {"court": [], "plaintiff": [], "respondent": []}
-        print(len(entries))
         for e in entries:
             plaintiff = e.find(find_plaintiff)
             if plaintiff:
-                plt_data = plaintiff.find(find_data).get_text()
-                info = []
-                for d in plt_data.split('\n'):
-                    filtered = filter(d)
-                    if filtered != "":
-                        info.append(filtered)
-                data["plaintiff"].append({'name': info[0], 'address': info[1], 'id': info[2]})
+                info = involved_info(plaintiff)
+                data["plaintiff"].append({"name": info[0], "address": info[1], "id": info[2]})
+            respondent = e.find(find_respondent)
+            if respondent:
+                info = involved_info(respondent)
+                data["respondent"].append({"name": info[0], "address": info[1], "id": info[2]})
+            court = e.find(find_court)
+            if court:
+                court = court.find("div")
+                divs = court.find_all("div")
+                info = {"judge": "", "court": ""}
+                if len(divs) == 2:
+                    info["judge"] = clean(divs[0].get_text())
+                    info["court"] = clean(divs[1].get_text())
+                else:
+                    info["court"] = clean(divs[0].get_text())
+                data["court"].append(info)
+            num = e.find(find_date)
+            if data:
+                num = num.find("div")
+                date = clean(num.find("div").get_text())
+                case = clean(num.find("a").get_text())
+                data["date"] = {"date": date, "case": case}
+
+    name = fname.split('.')[0]
+    with open(f"{name}.json", "w") as file:
+        json.dump(data, file)
 
 
 if __name__ == "__main__":
-    parse_civil()
+    parse("bankrupt.html")
